8c254b21de593ae6fb75b594a71becc22b24da02
Piotr Migdal <pmigdal@gmail.com>
2024-03-24 12:00:40 +0100

    me page

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="flex">
+      <div class="flex-column text">
+        <ContentDoc path="/text-components/me" />
+      </div>
+      <div class="flex-column image">
+        <!-- Example image, replace with your own -->
+        <img
+          class="main-img"
+          alt="A photo of Piotr Migdał"
+          src="~/assets/imgs/piotr-migdal-direct-smiling-2022-by-cytacka-600px.jpg"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.flex {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+
+.flex-column {
+  flex-grow: 1;
+  flex-shrink: 1;
+  flex-basis: 300px;
+}
+
+.main-img {
+  max-width: 100%;
+}
+</style>
