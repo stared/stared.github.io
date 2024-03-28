e933d539523fd45db00187e410fdc9beb9157fe4
Piotr Migdal <pmigdal@gmail.com>
2024-03-28 21:58:40 +0100

    blog post

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,70 @@
 <template>
   <main>
-    <ContentDoc />
+    <ContentDoc v-slot="{ doc }">
+      <article>
+        <div class="markdown-header">
+          <h1>{{ doc.title }}</h1>
+          <p class="header-information">
+            {{ formatDate(doc.date) }} | by Piotr Migdał
+            <span v-if="doc.medium_url">
+              &nbsp;| <a :href="doc.medium_url">originally posted at Medium</a>
+            </span>
+          </p>
+          <ul
+            v-if="doc.mentions && doc.mentions.length > 0"
+            class="header-mentions"
+          >
+            <li v-for="mention in doc.mentions" :key="mention.href">
+              <a :href="mention.href">{{ mention.text }}</a>
+            </li>
+          </ul>
+        </div>
+        <ContentRenderer :value="doc" />
+      </article>
+    </ContentDoc>
+    <footer>
+      By <NuxtLink to="/">Piotr Migdał</NuxtLink>, a curious being, doctor of
+      sorcery. See <NuxtLink to="/blog">my other blog posts</NuxtLink>.
+    </footer>
   </main>
 </template>
+
+<script setup lang="ts">
+const formatDate = (date: string) => {
+  return new Date(date).toLocaleDateString("en-UK", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+};
+</script>
+
+<style>
+.markdown-header .header-mentions {
+  font-size: 0.8em;
+}
+
+.markdown blockquote {
+  font-style: italic;
+}
+
+.markdown img {
+  max-width: 100%;
+  margin: auto;
+  display: block;
+}
+
+/* displaying only .katex-mathml */
+.markdown .katex .katex-html {
+  display: none;
+}
+
+p:has(> span.katex-display) {
+  text-align: center;
+}
+
+footer {
+  margin-top: 2em;
+  margin-bottom: 2em;
+}
+</style>
