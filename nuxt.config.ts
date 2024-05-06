572770896fea0be91ae530cf988f528b5d7bb237
Piotr Migdal <pmigdal@gmail.com>
2024-05-06 17:22:52 +0200

    turned a bad workaround into a better one

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,12 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["nuxt-content-assets", "@nuxt/content"],
+  hooks: {
+    close: () => {
+      // fixes `nuxi generate` hanging at the end "You can preview this build"
+      // @see https://github.com/nuxt/cli/issues/169#issuecomment-1729300497
+      // Workaround for https://github.com/nuxt/cli/issues/169
+      process.exit(0);
+    },
+  },
 });
