c2008a1393d58c200f8c892fd5062183d682cc61
Piotr Migdal <pmigdal@gmail.com>
2024-05-07 00:55:28 +0200

    redirects from old Jekyll pattern

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,53 @@ export default defineNuxtConfig({
       },
     },
   },
+  nitro: {
+    prerender: {
+      routes: [
+        "/2017/01/06/king-man-woman-queen-why.html",
+        "/2017/04/30/teaching-deep-learning.html",
+        "/2016/08/15/quantum-mechanics-for-high-school-students.html",
+        "/2015/12/14/sci-to-data-sci.html",
+        "/2016/03/15/data-science-intro-for-math-phys-background.html",
+        "/2017/09/30/dating-for-nerds-gender-differences.html",
+        "/2017/07/23/dating-for-nerds.html",
+        "/2018/09/15/simple-diagrams-deep-learning.html",
+        "/2017/08/14/bangbangcon.html",
+      ],
+    },
+  },
+
+  routeRules: {
+    // Redirect specific posts
+    // Legacy links from Jekyll times, still used online
+    "/2017/01/06/king-man-woman-queen-why.html": {
+      redirect: "/blog/2017/01/king-man-woman-queen-why",
+    },
+    "/2017/04/30/teaching-deep-learning.html": {
+      redirect: "/blog/2017/04/teaching-deep-learning",
+    },
+    "/2016/08/15/quantum-mechanics-for-high-school-students.html": {
+      redirect: "/blog/2016/08/quantum-mechanics-for-high-school-students",
+    },
+    "/2015/12/14/sci-to-data-sci.html": {
+      redirect: "/blog/2015/12/sci-to-data-sci",
+    },
+    "/2016/03/15/data-science-intro-for-math-phys-background.html": {
+      redirect: "/blog/2016/03/data-science-intro-for-math-phys-background",
+    },
+    "/2017/09/30/dating-for-nerds-gender-differences.html": {
+      redirect: "/blog/2017/09/dating-for-nerds-gender-differences",
+    },
+    "/2017/07/23/dating-for-nerds.html": {
+      redirect: "/blog/2017/07/dating-for-nerds",
+    },
+    "/2018/09/15/simple-diagrams-deep-learning.html": {
+      redirect: "/blog/2018/09/simple-diagrams-deep-learning",
+    },
+    "/2017/08/14/bangbangcon.html": {
+      redirect: "/blog/2017/08/bangbangcon",
+    },
+  },
   hooks: {
     close: () => {
       // fixes `nuxi generate` hanging at the end "You can preview this build"
